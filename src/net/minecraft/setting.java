47fe214ffc71fba796101699c0af2fa50ec88eb9
Arthur Halma <artur@art-studio.lviv.ua>
2012-08-04 15:23:57 +0300

    created (v 0.0.0)

---
+++ src/net/minecraft/setting.java
@@ -0,0 +1,20 @@
+package net.minecraft;
+
+public class setting {
+	public static String siteLink = "http://192.168.1.212";
+	 public static String forumLink = "http://192.168.1.212/";
+	 public static String uslugiLink = "http://imperia-mc.ru/donat.html";
+	 public static String monitorLink = "http://gengine/monitor/monitorLaunch.php";
+	 public static String hashLink = "http://192.168.1.212/hash.php?hash=";
+	 public static String authLink = "http://192.168.1.212/loginServer.php?user=";
+	 public static String loadLink = "http://s3.amazonaws.com/MinecraftDownload/";
+	 public static String skinsLink = "http://192.168.1.212/skin2d.php?skinpath=";
+	 public static String registerLink = "http://192.168.1.212/registerServer.php?user=";
+	 public static String newsUrl = "file:///F:/Documents%20and%20Settings/Freedom/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/news.htm";
+	 public static String catalogSkinsUrl = "http://www.minecraftskins.com/";
+	 public static String mineFolder = "artcraft";//????? ? ??????? ????????? ????????? (.minecraft)
+	 public static String downLaunchLink = "http://www.minecraft.net/download.jsp";
+	 public static String client1 = "minecraft";
+	 public static String client2 = "minecraftLight";
+	 public static String loadLinkUrlSkins = "http://192.168.1.212/skinLoad.php";
+}
